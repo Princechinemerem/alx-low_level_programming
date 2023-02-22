0f28f1cd93742f45b707ec35079765092c40c214
Prince Chinemerem Nwuju <priincenwuju01@gmail.com>
2023-02-22 01:13:31 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
 /**
- * main - Entry point
+ * main - Write a program that prints Holberton, followed by a new*
  *
- * Return: Always 0 (Success)
+ *
+ * Return: On success 0.
  */
 int main(void)
 {
-	char *sh = "main.h";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
 
 	return (0);
